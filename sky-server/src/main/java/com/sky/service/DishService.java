f3fa0697f3f2aee4d9ef14d2203c6a445e422244
yunfeidog <1844025705@qq.com>
2023-10-08 14:15:29 +0800

    菜品分页查询

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,8 +1,13 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 
 public interface DishService {
 
     public void save(DishDTO dishDTO);
+
+
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
